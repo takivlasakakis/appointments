aec8e216f488e6eaeeb060da567ab2a19e91c0be
macroscopic <mac.roche@gmail.com>
2016-08-03 15:22:30 -0700

    commit before deploying to heroku

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,7 @@ class Review < ActiveRecord::Base
   belongs_to :author, class_name: "User", foreign_key: "author_id"
   belongs_to :subject, class_name: "User", foreign_key: "subject_id"
   belongs_to :appointment, foreign_key: "appointment_id"
+
+  validates :author_id 
+
 end
