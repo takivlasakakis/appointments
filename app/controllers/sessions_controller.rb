dc684973ba4b07726aa61ea41318fb2fcda01313
Calvin Kwong <ckwong93@gmail.com>
2016-08-03 16:36:13 -0700

    Finish login/register

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,14 +7,13 @@
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to '/'
+      redirect_to '/sessions/show'
     else
       redirect_to '/login'
     end
   end
 
   def show
-    @user = User.find(params[:id])
   end
 
   def destroy
