b72d11e0a76835eed2ca61810c412d1ed68b54c6
macroscopic <mac.roche@gmail.com>
2016-08-03 15:39:33 -0700

    hello world to heroku

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,6 +3,6 @@ class Review < ActiveRecord::Base
   belongs_to :subject, class_name: "User", foreign_key: "subject_id"
   belongs_to :appointment, foreign_key: "appointment_id"
 
-  validates :author_id 
+  validates :author_id, :subject_id, :appointmnet_id, presence: true 
 
 end
