587a5be16aabbfebd40d77888f2053f32d75a085
macroscopic <mac.roche@gmail.com>
2016-08-04 15:05:53 -0700

    fixed routes even more, lots of playing with where views should go. Profile view gaining functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,18 +7,10 @@
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to '/sessions/show'
+      redirect_to '/profile'
     else
       redirect_to '/login'
     end
-  end
-
-  def show
-    p ">>>>>>>>>>>>>>>>>>>"
-    p @current_user
-    p current_user
-    @user = @current_user
-    @appointments = Appointment.where('student_id = ? OR mentor_id = ?', @current_user.id, @current_user.id)
   end
 
   def destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,5 @@
 class UsersController < ApplicationController
 
-  def profile
-    @user = current_user
-    redirect :'/users/show'
-  end
-
   def new
   end
 
@@ -22,15 +17,13 @@ class UsersController < ApplicationController
   end
 
   def show
-  	@user = User.find(params[:id])
+    @user = User.find(session[:user_id])
+    @appointments = Appointment.where("mentor_id = ? OR student_id = ?", @user.id, @user.id)
   end
 
   private
   def user_params
     params.require(:user).permit(:username, :password, :email, :role)
   end
-
-
-
 
 end
