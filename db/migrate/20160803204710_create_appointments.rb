f4fc2dc4529be9e1fb75aa411a70538a79f866ef
Calvin Kwong <ckwong93@gmail.com>
2016-08-05 12:25:11 -0700

    remove driver code

--- db/migrate/20160803204710_create_appointments.rb
+++ db/migrate/20160803204710_create_appointments.rb
@@ -11,16 +11,3 @@ class CreateAppointments < ActiveRecord::Migration
   end
 end
 
-
- #  <ul>
- #  <% @past_appointments.each do |appoint|%>
- #  <br><li><a href="/appointments/<%=appoint.id%>"><%= appoint.appointment_time %></a></li>
- #  Assigned Mentor: <%= User.find(appoint.mentor_id).username %><br>
- #   <% if appoint.student_id == nil %>
- #  Assigned Student: <%= p "not yet determined" %><br>
- #  Message: <%= appoint.message %>
- # <% else %>
- #  Assigned Student: <%= User.find(appoint.mentor).username %>
- # <% end %><br>
-
- #  <% end %></ul>
