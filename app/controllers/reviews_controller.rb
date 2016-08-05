c35cf36a6ac7fa382e221b7505e8f4d03bd22cfb
Taki Vlasakakis <taki.vlasakakis@gmail.com>
2016-08-04 17:20:56 -0700

    commit to pull fresh changes off of dev

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,10 +6,16 @@ class ReviewsController < ApplicationController
 
 	def create
 	  @review = Review.new(user_params)
-		
+		@review.author_id = current_user.id
+		redirect_to reviews_path
 	end
 
 	def show
 	  @review = Review.find(params[:id])
+	end
+
+	private
+	def user_params
+		params.require(:reviews).permit(:author_id, :subject_id, :stars, :body, :appointment_id)
 	end
 end
